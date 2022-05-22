ee794afd9e5c8471511cf470a918f18e3057e9e5
r1411 <roma00055@gmail.com>
2022-05-22 23:11:02 +0300

    Поддержка добавления персонажей (без ввода имени)

--- BSAkinator/GameState.cs
+++ BSAkinator/GameState.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BSAkinator
+﻿namespace BSAkinator
 {
     public enum GameState { 
         Init,
         Asking,
         Guessed,
-        NoAnswer
+        NoAnswer,
+        AskingToAdd
     }
 }
