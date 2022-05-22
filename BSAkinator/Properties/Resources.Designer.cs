6aa47043def78bc3d3c21866b1b142d449334316
r1411 <roma00055@gmail.com>
2022-05-23 01:09:48 +0300

    Поддержка картинок

--- BSAkinator/Properties/Resources.Designer.cs
+++ BSAkinator/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace BSAkinator.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bs_loading {
+            get {
+                object obj = ResourceManager.GetObject("bs_loading", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap defi {
             get {
                 object obj = ResourceManager.GetObject("defi", resourceCulture);
